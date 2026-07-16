9a7ff25b9a51a1c471168f3f0718870aae51e714
Endry Lundy <clansdoli@gmail.com>
2026-07-16 05:32:56 +0200

    updated env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://aqiwatrc9qiwm4e4jzdyu6jm.164.132.104.78.sslip.io',
+  apiUrl: 'https://admin-api.evangelisation-charisma.fr',
 };
